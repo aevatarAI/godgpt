89baa3a131191f491cd23e3312a1a7c30f0ee35b
neo887 <yunfeng.song@aelf.io>
2025-06-27 19:42:24 +0800

    feat: remove test code

--- src/GodGPT.GAgents/Invitation/InvitationGAgent.cs
+++ src/GodGPT.GAgents/Invitation/InvitationGAgent.cs
@@ -201,9 +201,7 @@ public class InvitationGAgent : GAgentBase<InvitationState, InvitationLogEvent>,
                 Credits = credits,
                 RewardType = RewardTypeEnum.SubscriptionReward,
                 IsScheduled = true,
-                //TODO Testing
-                //ScheduledDate = DateTime.UtcNow.AddDays(30),
-                ScheduledDate = DateTime.UtcNow.AddMinutes(30),
+                ScheduledDate = DateTime.UtcNow.AddDays(30),
                 InvoiceId = invoiceId
             });
             await ConfirmEvents();
