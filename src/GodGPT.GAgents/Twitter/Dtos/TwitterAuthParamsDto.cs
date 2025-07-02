69908bb383593935883de0be99eb3124955245d5
neo887 <yunfeng.song@aelf.io>
2025-07-02 18:27:57 +0800

    feat: remove redirectUri

--- src/GodGPT.GAgents/Twitter/Dtos/TwitterAuthParamsDto.cs
+++ src/GodGPT.GAgents/Twitter/Dtos/TwitterAuthParamsDto.cs
@@ -5,10 +5,9 @@ public class TwitterAuthParamsDto
 {
     [Id(1)] public string ClientId { get; set; }
     [Id(2)] public string GrantType { get; set; }
-    [Id(3)] public string RedirectUri { get; set; }
-    [Id(4)] public string CodeChallenge { get; set; }
-    [Id(5)] public string CodeChallengeMethod { get; set; }
-    [Id(6)] public string State { get; set; }
-    [Id(7)] public string Scope { get; set; }
-    [Id(8)] public string ResponseType { get; set; }
+    [Id(3)] public string CodeChallenge { get; set; }
+    [Id(4)] public string CodeChallengeMethod { get; set; }
+    [Id(5)] public string State { get; set; }
+    [Id(6)] public string Scope { get; set; }
+    [Id(7)] public string ResponseType { get; set; }
 }
