59be299b3ca874134c9dfa0d6b3324c309420947
yann <feng.yan@aelf.io>
2025-07-16 11:25:11 +0800

    update RateLimitOptions

--- src/GodGPT.GAgents/Common/Options/RateLimitOptions.cs
+++ src/GodGPT.GAgents/Common/Options/RateLimitOptions.cs
@@ -4,14 +4,14 @@ namespace Aevatar.Application.Grains.Common.Options;
 public class RateLimitOptions
 {
     // Text conversation rate limits
-    [Id(0)] public int UserMaxRequests { get; set; } = 25;
-    [Id(1)] public int UserTimeWindowSeconds { get; set; } = 60 * 60 * 3; //s
-    [Id(2)] public int SubscribedUserMaxRequests { get; set; } = 50;
-    [Id(3)] public int SubscribedUserTimeWindowSeconds { get; set; } = 60 * 60 * 3; //s
+    [Id(0)] public int UserMaxRequests { get; set; } = 10;//25;
+    [Id(1)] public int UserTimeWindowSeconds { get; set; } = 60 * 20;//60 * 60 * 3; //s
+    [Id(2)] public int SubscribedUserMaxRequests { get; set; } = 15;//50;
+    [Id(3)] public int SubscribedUserTimeWindowSeconds { get; set; } = 60 * 10;//60 * 60 * 3; //s
     
     // Voice conversation rate limits
-    [Id(4)] public int VoiceUserMaxRequests { get; set; } = 10; 
-    [Id(5)] public int VoiceUserTimeWindowSeconds { get; set; } = 60 * 60 * 3; //s
-    [Id(6)] public int VoiceSubscribedUserMaxRequests { get; set; } = 50; 
-    [Id(7)] public int VoiceSubscribedUserTimeWindowSeconds { get; set; } = 60 * 60 * 3; //s
+    [Id(4)] public int VoiceUserMaxRequests { get; set; } = 5;//10; 
+    [Id(5)] public int VoiceUserTimeWindowSeconds { get; set; } = 60 * 10;//60 * 60 * 3; //s
+    [Id(6)] public int VoiceSubscribedUserMaxRequests { get; set; } = 8;//50; 
+    [Id(7)] public int VoiceSubscribedUserTimeWindowSeconds { get; set; } =  60 * 10;//60 * 60 * 3; //s
 }
