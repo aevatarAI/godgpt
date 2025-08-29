23fad932b1a0d74d627f6984cba848ee31f0f53a
kevin <kaiwen.zheng@aelf.io>
2025-08-29 16:14:43 +0800

    Fix Orleans serialization for TimezoneDeviceInfo
    - Add [GenerateSerializer] attribute to TimezoneDeviceInfo class
    - Add [Id(x)] attributes to all properties for Orleans serialization
    - Resolves 'Could not find a copier for TimezoneCoordinatorInfo' error
    - Fixes timezone ecosystem initialization failures

--- src/GodGPT.GAgents/DailyPush/TimezoneDeviceInfo.cs
+++ src/GodGPT.GAgents/DailyPush/TimezoneDeviceInfo.cs
@@ -5,17 +5,18 @@ namespace GodGPT.GAgents.DailyPush;
 /// <summary>
 /// Device information in timezone for debugging - TODO: Remove before production
 /// </summary>
+[GenerateSerializer]
 public class TimezoneDeviceInfo
 {
-    public Guid UserId { get; set; }
-    public string DeviceId { get; set; } = "";
-    public string PushToken { get; set; } = "";
-    public string TimeZoneId { get; set; } = "";
-    public string PushLanguage { get; set; } = "";
-    public bool PushEnabled { get; set; }
-    public DateTime RegisteredAt { get; set; }
-    public DateTime LastTokenUpdate { get; set; }
-    public bool HasEnabledDeviceInTimezone { get; set; }
-    public int TotalDeviceCount { get; set; }
-    public int EnabledDeviceCount { get; set; }
+    [Id(0)] public Guid UserId { get; set; }
+    [Id(1)] public string DeviceId { get; set; } = "";
+    [Id(2)] public string PushToken { get; set; } = "";
+    [Id(3)] public string TimeZoneId { get; set; } = "";
+    [Id(4)] public string PushLanguage { get; set; } = "";
+    [Id(5)] public bool PushEnabled { get; set; }
+    [Id(6)] public DateTime RegisteredAt { get; set; }
+    [Id(7)] public DateTime LastTokenUpdate { get; set; }
+    [Id(8)] public bool HasEnabledDeviceInTimezone { get; set; }
+    [Id(9)] public int TotalDeviceCount { get; set; }
+    [Id(10)] public int EnabledDeviceCount { get; set; }
 }
